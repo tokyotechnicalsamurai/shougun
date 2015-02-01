911685662f9add603b29cecb7357175d4813e454
sh4869 <nobuk4869@gmail.com>
2015-02-02 02:17:01 +0900

    Set DriveSubsystem to CommandBase.cpp

--- src/CommandBase.cpp
+++ src/CommandBase.cpp
@@ -1,9 +1,10 @@
+#include "Subsystems/DriveSubsystem.h"
 #include "CommandBase.h"
-#include "Subsystems/ExampleSubsystem.h"
 #include "Commands/Scheduler.h"
 
 // Initialize a single static instance of all of your subsystems to NULL
-ExampleSubsystem* CommandBase::examplesubsystem = NULL;
+DriveSubsystem CommandBase::DriveSubsystem = NULL;
+
 OI* CommandBase::oi = NULL;
 
 CommandBase::CommandBase(char const *name) :
@@ -21,7 +22,8 @@ void CommandBase::init()
 {
 	// Create a single static instance of all of your subsystems. The following
 	// line should be repeated for each subsystem in the project.
-	examplesubsystem = new ExampleSubsystem();
 
 	oi = new OI();
+
+	DriveSubsystem = new DriveSubsystem();
 }
