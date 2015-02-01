3dbde8faa8655e4fcfb89c477e2c96a86ad20ad5
sh4869 <nobuk4869@gmail.com>
2015-02-01 14:13:52 +0900

    Add DriveSubsystem

---
+++ src/Subsystems/DriveSubsystem.h
@@ -0,0 +1,17 @@
+#ifndef DriveSubsystem_H
+#define DriveSubsystem_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+class DriveSubsystem: public Subsystem
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+public:
+	DriveSubsystem();
+	void InitDefaultCommand();
+};
+
+#endif
