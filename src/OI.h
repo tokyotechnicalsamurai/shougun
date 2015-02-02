462cf17a56979aafaead2e19ea8bc0e82df66a3d
sh4869 <nobuk4869@gmail.com>
2015-02-02 17:11:31 +0900

    Create GetXplusY function and GetXminusY function

--- src/OI.h
+++ src/OI.h
@@ -12,9 +12,26 @@ private:
 
 public:
 	OI();
+	inline float GetXplusY(){
+		float val;
+		val = FractionOmitted(Joystick.GetY() + Joystick.GetX());
+		if(val > 1.2) val = 1.2;
+		if(val < -1.2) val = -1.2;
+		return val;
+	}
+	inline float GetXminusY(){
+		float val;
+		val = FractionOmitted(Joystick.GetY() - Joystick.GetX());
+		if(val > 1.2) val = 1.2;
+		if(val < -1.2) val = -1.2;
+		return val;
+	}
+
+
 	inline float GetStickX(){ return FractionOmitted(Joystick.GetX()); }
 	inline float GetStickY(){ return FractionOmitted(Joystick.GetY()); }
 	inline float GetStickZ(){ return FractionOmitted(Joystick.GetZ()); }
+
 
 	inline float FractionOmitted(float original){
 		if(fabsf(original) < 0.01 ){
