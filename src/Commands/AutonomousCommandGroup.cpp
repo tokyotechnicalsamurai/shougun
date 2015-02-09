620459f60b6422bcbd12d199380499df3041124f
sh4869 <nobuk4869@gmail.com>
2015-02-10 00:18:36 +0900

    Set order of Autonomous Commands in CommandGroup

--- src/Commands/AutonomousCommandGroup.cpp
+++ src/Commands/AutonomousCommandGroup.cpp
@@ -1,7 +1,17 @@
 #include "AutonomousCommandGroup.h"
+
+#include "RaiseContainer.h"
+#include "DownContainer.h"
+#include "RaiseTrash.h"
+#include "DownTrash.h"
+#include "MoveContainer.h"
 #include "../RobotMap.h"
 
 AutonomousCommandGroup::AutonomousCommandGroup()
 {
-
+	AddSequential(new RaiseTrash());
+	AddSequential(new DownTrash());
+	AddSequential(new RaiseContainer());
+	AddSequential(new MoveContainer());
+	AddSequential(new DownContainer());
 }
