a3089fde01cbdec54bcd3c7b76c5263e3dad7535
sh4869 <nobuk4869@gmail.com>
2015-02-11 18:09:56 +0900

    Fix Move2Container

--- src/Commands/Move2Container.cpp
+++ src/Commands/Move2Container.cpp
@@ -4,6 +4,7 @@ Move2Container::Move2Container()
 {
 	Requires(driveSubsystem);
 	Requires(sensorSubsystem);
+	isCenter = false;
 }
 
 // Called just before this Command runs the first time
@@ -15,28 +16,29 @@ void Move2Container::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void Move2Container::Execute()
 {
-	while(sensorSubsystem->GetDistLeft() > 10.0 && sensorSubsystem->GetDistRight() > 10.0){
-		driveSubsystem->DriveMotors(0.3,-0.3,-0.3,0.3);
+	if(sensorSubsystem->GetDistLeft() <= 10.0 && sensorSubsystem->GetDistRight() <= 10.0){
+		isCenter = false;
+		SetTimeout(0.5);
 	}
 	driveSubsystem->DriveMotors(0.3,-0.3,-0.3,0.3);
-	Wait(0.5);
+
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool Move2Container::IsFinished()
 {
-	return false;
+	return isCenter && IsTimedOut();
 }
 
 // Called once after isFinished returns true
 void Move2Container::End()
 {
-
+	driveSubsystem->Stop();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void Move2Container::Interrupted()
 {
-
+	driveSubsystem->Stop();
 }
--- src/Commands/Move2Container.h
+++ src/Commands/Move2Container.h
@@ -13,6 +13,8 @@ public:
 	bool IsFinished();
 	void End();
 	void Interrupted();
+private:
+	bool isCenter;
 };
 
 #endif
