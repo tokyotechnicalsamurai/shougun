fccddf7a51e0bf830819fb928d226e672fbfd2c4
sh4869 <nobuk4869@gmail.com>
2015-02-11 18:22:37 +0900

    Fix BringBurden Command

--- src/Commands/BringBurden.cpp
+++ src/Commands/BringBurden.cpp
@@ -8,26 +8,25 @@ BringBurden::BringBurden()
 // Called just before this Command runs the first time
 void BringBurden::Initialize()
 {
-
+	SetTimeout(5.0);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void BringBurden::Execute()
 {
 	driveSubsystem->DriveMotors(0.3,0.3,0.3,0.3);
-	Wait(5.0); // kimeuti
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool BringBurden::IsFinished()
 {
-	return false;
+	return IsTimedOut();
 }
 
 // Called once after isFinished returns true
 void BringBurden::End()
 {
-
+	driveSubsystem->Stop();
 }
 
 // Called when another command which requires one or more of the same
