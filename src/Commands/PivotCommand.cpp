c72044e10e116939b5f19486152d28f66eb555d1
sana37 <deadoraliveoflife.@gmail.com>
2015-02-15 03:18:43 +0900

    improve gyro

--- src/Commands/PivotCommand.cpp
+++ src/Commands/PivotCommand.cpp
@@ -1,26 +1,22 @@
 #include "PivotCommand.h"
 
-PivotCommand::PivotCommand(bool isRight,int Angle)
+PivotCommand::PivotCommand(float Angle , bool state , bool state2)
 {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(chassis);
 	Requires(driveSubsystem);
 	Requires(sensorSubsystem);
-	isRightPovit = isRight;
 
 	isFinishPovit = false;
-	/*
 	povitAngle = Angle;
-	clock.Start();
-	baseTime = base = 0;
-	for(short i = 0;i < 10;i++){
-		base += gyro.GetVoltage();
+	breaktime = abs(Angle);
+	if(povitAngle > 0){
+		isRightPovit = false;
+	}else{
+		isRightPovit = true;
 	}
-	base /= 10.0;
-	baseTime = clock.Get();
-	deg = 0;
-	prespeed = 0;
-	*/
+	parallel_state = state;
+	dist_state = state2;
 }
 
 // Called just before this Command runs the first time
@@ -32,39 +28,77 @@ void PivotCommand::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void PivotCommand::Execute()
 {
-	/*float data = 0;
-	float time;
-	float speed;
-	*/
+	Timer* clock = new Timer();
+	float deg , base , prespeed , beforetime , starttime;
+
+	clock->Start();
+	deg = 0;
+	base = 0;
+	prespeed = 0;
+	for(short i = 0  ;  i < 10  ;  i++){
+		base += sensorSubsystem->GetGyro();
+	}
+	base /= 10.0;
+	starttime = clock->Get();
+	beforetime = clock->Get();
+
 	while(!isFinishPovit || !IsTimedOut()){
-		/*
+		float data = 0;
+		float time , speed;
+
 		for(short i = 0  ;  i < 10  ;  i++){
-			data += gyro.GetVoltage();
+			data += sensorSubsystem->GetGyro();
 		}
 		data /= 10.0;
 		data -= base;
 		if(-0.006 < data  &&  data < 0.006) data = 0;
 		speed = data / 0.0067;
-		time = clock.Get() - baseTime;
-		baseTime = clock.Get();
+		time = clock->Get() - beforetime;
+		beforetime = clock->Get();
 		deg += (prespeed + speed) / 2.0 * time;
 		prespeed = speed;
 		std::cout << deg << std::endl;
-		*/
-		if(isRightPovit){
-			if(sensorSubsystem->GetDegree() > -90){
-				driveSubsystem->DriveMotors(SPEED,-SPEED,SPEED,-SPEED);
+
+		if(!parallel_state){
+			if(isRightPovit){
+				if(sensorSubsystem->GetDegree() > -90){
+					driveSubsystem->DriveMotors(SPEED,-SPEED,SPEED,-SPEED);
+				}else{
+					isFinishPovit = true;
+				}
 			}else{
-				isFinishPovit = true;
+				if(sensorSubsystem->GetDegree() < 90){
+					driveSubsystem->DriveMotors(-SPEED,SPEED,-SPEED,SPEED);
+				}else{
+					isFinishPovit = true;
+				}
 			}
 		}else{
-			if(sensorSubsystem->GetDegree() < 90){
-				driveSubsystem->DriveMotors(-SPEED,SPEED,-SPEED,SPEED);
+			float gap;
+			if(-10.0 < deg  &&  deg < 10.0){
+				gap = deg * 0.01;
 			}else{
-				isFinishPovit = true;
+				gap = ((gap > 0) ? 0.1 : -0.1);
+			}
+			if(isRightPovit){
+				driveSubsystem->DriveMotors(SPEED + gap , -SPEED + gap , -SPEED + gap , SPEED + gap);
+			}else{
+				driveSubsystem->DriveMotors(-SPEED + gap , SPEED + gap , SPEED + gap , -SPEED + gap);
+			}
+			if(IsTimedOut()  ||  clock->Get() - starttime > 2.0) break;
+			if(dist_state){
+				float left , right;
+				left = sensorSubsystem->GetDistLeft();
+				right = sensorSubsystem->GetDistRight();
+				if(left <= CONTAINERDIST  &&  right <= CONTAINERDIST){
+					break;
+				}
 			}
 		}
+
+		Wait(0.01);
 	}
+	std::cout << "finish\n";
 
 }
 
--- src/Commands/PivotCommand.h
+++ src/Commands/PivotCommand.h
@@ -7,10 +7,14 @@
 class PivotCommand: public CommandBase
 {
 private:
+	float povitAngle;
+	float breaktime;
 	bool isRightPovit,isFinishPovit;
+	bool parallel_state;//true:parallel movement , false:turn setted degree
+	bool dist_state;//true:look distance and stop , false:not look distance
 
 public:
-	PivotCommand(bool isRight,int Angle);
+	PivotCommand(float , bool = false , bool = false);
 	void Initialize();
 	void Execute();
 	bool IsFinished();
